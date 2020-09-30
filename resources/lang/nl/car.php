a7ba1e6e8356c7f4d1f5bc76bd709291fbb0c74b
dimmaa <dimma.hope@yandex.com>
2020-09-30 18:19:57 +0200

    make it better

---
+++ resources/lang/nl/car.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'employer'=>"04-2019 t/m 10-2020, ProActive(software)",
+    'ex-em'=>"ProActive is een software bedrijf in Haarlem. De software die dit bedrijf ontwikkelt heet de SpendCloud. De spendcloud is een applicatie voor organisaties om hun uitgaven te beheren. ",
+    'ex-em-daily'=>"Bij Proactive werken ze met de Scrum-methodieken met onder andere: 2 weekse sprints, dagelijkse stand ups, wekelijkse refinements en sprintdemo's. Samen met mijn team verantwoordelijk voor 2 van de grootste en oudste modules van de applicatie. Mijn Dagelijkse taken: refactoren van legacy code, foutmeldingen debuggen, SQL queries schrijven, nieuwe functionaliteiten implementeren, code reviewen, documentaties schrijven en communiceren met support ",
+    'learned'=>"Wat ik heb geleerd bij ProActive",
+    'learned-expand'=>"Bij proactive heb ik geleerd om productief volgens de scrum technieken samen te werken. En heb daarbij leren omgaan met handige tools die belangrijk zijn in een enterprise development omgeving. Door het hoge aantal gebruikers is het essentieel dat de applicatie foutloos draait daarom is het van uiterst belang om fouten in de code te voorkomen. Git is hiervoor de perfecte tool om je applicatie in delen te updaten,  ",
+    ''=>"",
+    ''=>"",
+    ''=>"",
+];
--- resources/views/home/car.blade.php
+++ resources/views/home/car.blade.php
@@ -1,20 +1,22 @@
 @extends('layouts.app')
 <div class="container">
-    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel" style="padding-top: 100px;">
         <ol class="carousel-indicators">
             <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
             <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
             <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
         </ol>
         <div class="carousel-inner">
-            <div class="carousel-item active">
-                <img class="d-block w-100" src="https://www.computerplan.nl/file/413/partner-software-proactive.png?width=343&height=212" alt="First slide">
+            <div class="carousel-item active" style="height: 70vh; width:100%">
+                <img class="d-block w-100" src="https://lh3.googleusercontent.com/9PTLz2InSxaRrp07tUUjUgr42KZWNEldvo__sNgs5qGnVgcDbHge6hV_y0Xk-WBkmhsg" alt="First slide" style="height: 100%">
+                <a href="#" data-anchor="#highlights"><button class="btn btn-primary btn-marketing rounded-pill lift lift-sm">Read More</button></a>
+
             </div>
-            <div class="carousel-item">
-                <img class="d-block w-100" src="https://www.marketing-communicatie-vacatures.nl/uploads/afbeeldingen/vacatures/Bedrijfsprofielen/ProActive%20Software%20Nederland/ProActive%20Software%20Nederland%20banner1.jpg" alt="Second slide">
+            <div class="carousel-item" style="height: 70vh; width: 100%">
+                <img class="d-block w-100" src="https://www.marketing-communicatie-vacatures.nl/uploads/afbeeldingen/vacatures/Bedrijfsprofielen/ProActive%20Software%20Nederland/ProActive%20Software%20Nederland%20banner1.jpg" alt="Second slide" style="height: 100%; border-radius: 20px;">
             </div>
-            <div class="carousel-item">
-                <img class="d-block w-100" src="https://docplayer.nl/docs-images/91/107738640/images/7-0.jpg" alt="Third slide">
+            <div class="carousel-item" style="height: 70vh; width: 100%">
+                <img class="d-block w-100" src="https://docplayer.nl/docs-images/91/107738640/images/7-0.jpg" alt="Third slide" style="height: 100%">
             </div>
         </div>
         <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
@@ -26,7 +28,23 @@
             <span class="sr-only">Next</span>
         </a>
     </div>
-    <br><br>
+    <br>
+    <div class="page-header-content">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12" data-aos="fade">
+                    <h1 class="em-title">{{__('car.employer')}}</h1>
+                    <p class="em-defined">{{__('car.ex-em')}}</p>
+                    <p class="ex-daily">{{__('car.ex-em-daily')}}</p>
+                    <p class="page-header-text">{{__('car.learned')}}</p>
+                    <p class="page-header-text">{{__('car.learned-expand')}}</p>
+                    <a href="#" data-anchor="#highlights-proactive"><button class="btn btn-primary btn-marketing rounded-pill lift lift-sm">Read More</button></a>
+                </div>
+
+            </div>
+        </div>
+    </div>
+    <br>
     <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
         <ol class="carousel-indicators">
             <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
@@ -35,7 +53,7 @@
         </ol>
         <div class="carousel-inner">
             <div class="carousel-item active">
-                <img class="d-block w-100" src="..." alt="First slide">
+                <img class="d-block w-100" src="https://scontent-lga3-2.cdninstagram.com/v/t51.2885-15/e35/27890837_148531495844789_8221316382415912960_n.jpg?_nc_ht=scontent-lga3-2.cdninstagram.com&_nc_cat=105&_nc_ohc=lqHQLm_4fxEAX-b3Aoc&_nc_tp=18&oh=961f34fa3b55ced82e9d888e59866f5c&oe=5F9D861B" alt="First slide">
             </div>
             <div class="carousel-item">
                 <img class="d-block w-100" src="..." alt="Second slide">
