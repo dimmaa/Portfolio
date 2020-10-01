e516c27a8a3be5f4a14d0ffa760b11959422c59f
dimmaa <dimma.hope@yandex.com>
2020-10-01 06:56:35 +0200

    make it betteri

--- resources/views/home/dims.blade.php
+++ resources/views/home/dims.blade.php
@@ -18,6 +18,38 @@
 <div id="layoutDefault">
     <div id="layoutDefault_content">
         <main>
+            <nav class="navbar navbar-marketing navbar-expand-lg bg-dark navbar-light navbar-preview fixed-top">
+                <div class="container">
+                    <a class="navbar-brand text-primary" href="#"><span style="color: #ffffff;">Dimma</span></a><button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><i data-feather="menu"></i></button>
+                    <div  class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav ml-auto mr-lg-5" >
+                            <li class="nav-item"><a style="color: #ffffff;" class="nav-link" href="#" data-anchor="#about">{{__('nav.sk')}} </a></li>
+                            <li class="nav-item"><a style="color: #ffffff;" class="nav-link" href="#" data-anchor="#highlights">{{__('nav.ca')}} </a></li>
+                            <li class="nav-item"><a style="color: #ffffff;" class="nav-link" href="#" data-anchor="#contact">{{__('nav.ed')}}</a></li>
+                            <li class="nav-item dropdown no-caret">
+                                <a class="nav-link dropdown-toggle" id="navbarDropdownDocs" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">{{__('nav.la')}}<i class="fas fa-chevron-right dropdown-arrow"></i></a>
+                                <div class="dropdown-menu dropdown-menu-right animated--fade-in-up" aria-labelledby="navbarDropdownDocs">
+                                    <a class="dropdown-item py-3" href="#" target="_blank"
+                                    ><div class="icon-stack bg-primary-soft text-primary mr-4"><i class="fas fa-book-open"></i></div>
+                                        <div>
+                                            <div class="small text-gray-500">English</div>
+                                            EnFlag
+                                        </div></a>
+                                    <div class="dropdown-divider m-0"></div>
+                                    <a class="dropdown-item py-3" href="#" target="_blank">
+                                        <div class="icon-stack bg-primary-soft text-primary mr-4"><i class="fas fa-code"></i></div>
+                                        <div>
+                                            <div class="small text-gray-500">Nederlands</div>
+                                            nlFlag
+                                        </div></a>
+                                    <div class="dropdown-divider m-0"></div>
+                                </div>
+                            </li>
+                        </ul>
+                        <a class="btn-danger btn rounded-pill px-4 ml-lg-4" href="https://github.com/dimmaa/Portfolio" target="_blank">See code on Github<i class="fas fa-arrow-right ml-1"></i></a>
+                    </div>
+                </div>
+            </nav>
 
             <header class="page-header page-header-light bg-white">
                 <div class="page-header-content">
